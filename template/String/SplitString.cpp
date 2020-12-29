28f84c315d97d083be5fc7a7e887cd7ef7152ee5
Josephtao584 <506188980@qq.com>
2020-12-29 21:39:57 +0800

    ...

---
+++ template/String/SplitString.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Joseph on 12/27/2020.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//
+void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c)
+{
+    //input, output, split_string
+    std::string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while(std::string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2-pos1));
+
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
+
