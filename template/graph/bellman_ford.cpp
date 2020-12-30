183a64dc3198ea9729119068a30effeb753d3e7f
Josephtao584 <506188980@qq.com>
2020-12-30 19:21:48 +0800

    ...

---
+++ template/graph/bellman_ford.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joseph on 2020/12/30.
+//
+
+
--- template/graph/dijkstra_1.cpp
+++ template/graph/dijkstra_1.cpp
@@ -5,10 +5,11 @@
 #include <cstring>
 #include <algorithm>
 
-const int N = 510;
+const int N = 510, M = 100010;
 
 int n, m;
 int g[N][N];
+int h[N], e[M], ne[M], w[M], idx;
 int dist[N];
 bool st[N];
 
@@ -31,6 +32,28 @@ int dijkastra_1(){         //朴素版
     }
     if(dist[n] == 0x3f3f3f3f)
         return -1;
+    return dist[n];
+}
+
+void add(int a, int b, int c){
+    e[idx] = b; w[idx] = c; ne[idx] = h[a]; h[a] = idx++;
+}
+int dijkastra_2(){         //邻接矩阵版
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    for(int i = 0; i < n; i++){
+        int t = -1;
+        for(int j = 1; j <= n; j++){
+            if(!st[j] && (t == -1 || dist[t] > dist[j]))
+                t = j;
+        }
+        st[t] = true;
+        for(int j = h[t]; j != -1; j = ne[j]){
+            int u = e[j];
+            dist[u] = min(dist[u], dist[t] + w[j]);
+        }
+    }
+    if(dist[n] == 0x3f3f3f3f) return -1;
     return dist[n];
 }
 
