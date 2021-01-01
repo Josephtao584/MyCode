23bb6c4590af9badf933f43f42e8edc1f6523ce8
taowenpeng <506188980@qq.com>
2021-01-01 18:54:46 +0800

    ...

--- template/graph/floyd.cpp
+++ template/graph/floyd.cpp
@@ -1,5 +1,39 @@
-//
-// Created by 陶文鹏 on 2021/1/1.
-//
+#include<iostream>
+#include<cstring>
+#include<algorithm>
 
+using namespace std;
 
+const int N = 210, INF = 1e9;
+
+int g[N][N];
+int n, k, m;
+
+void floyd(){
+    for(int k = 1; k <= n; k++)
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
+}
+
+int main(){
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++){
+            if (i == j) g[i][j] = 0;
+            else g[i][j] = INF;
+        }
+    for(int i = 0; i < m; i++){
+        int a, b, w;
+        cin >> a >> b >> w;
+        g[a][b] = min(g[a][b], w);
+    }
+    floyd();
+    for(int i = 0; i < k; i++){
+        int a, b;
+        cin >> a >> b;
+        if(g[a][b] > INF / 2) puts("impossible");
+        else cout << g[a][b] << endl;
+    }
+
+}
