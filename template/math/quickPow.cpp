fc97289ef0486419c02573d8378aef549b02f9d5
Josephtao584 <506188980@qq.com>
2021-01-04 15:28:16 +0800

    ...

---
+++ template/math/quickPow.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+const long long mod = 1e9+7;
+
+ll quick_pow(ll a,ll x)
+{
+    ll ans = 1;
+    while(x){
+        if(x & 1) ans = ans * a % mod;
+        x >>= 1;
+        a = a * a % mod;
+    }
+    return ans;
+}
