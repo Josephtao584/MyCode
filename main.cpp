00912438346cd25f80e0760942c27571d97916bf
Josephtao584 <506188980@qq.com>
2021-01-04 19:56:55 +0800

    ...

--- main.cpp
+++ main.cpp
@@ -1,58 +1,21 @@
-//
-// Created by 陶文鹏 on 2020/12/31.
-//
 
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-
+#include<iostream>
 using namespace std;
+typedef long long ll;
+const long long mod = 1e9+7;
 
-const int N = 100010, M = 200010;
-
-struct Edge{
-    int a, b, w;
-    bool operator < (const Edge &u) const {
-        return w < u.w;
+ll quick_pow(ll a, ll x)
+{
+    ll ans = 1;
+    while(x){
+        if(x & 1) ans = ans * a % mod;
+        x >>= 1;
+        a = a * a % mod;
     }
-}edge[M];
-
-int n, m;
-int p[N];
-
-int find(int x){
-    if(p[x] != x) return p[x] = find(p[x]);
-    return x;
+    return ans;
 }
+
 
 int main(){
-    cin >> n >> m;
-    for(int i = 0; i < m; i++){
-        int a, b, w;
-        cin >> a >> b >> w;
-        edge[i] = {a, b, w};
-    }
-
-    sort(edge, edge + m);
-
-    for(int i = 1; i <= n; i++){
-        p[i] = i;
-    }
-
-    int res = 0, cnt = 0;
-
-    for(int i = 0; i < m; i++){
-        int a = edge[i].a, b = edge[i].b, w = edge[i].w;
-        int pa = find(a), pb = find(b);
-        if(pa != pb){
-            cnt++;
-            p[pa] = pb;
-            res += w;
-        }
-    }
-    if(cnt >= n - 1)
-        cout << res << endl;
-    else puts("impossible");
-    return 0;
+   cout << quick_pow(2, 0);
 }
-
---
+++ template/math/get_primes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 1000010;
+int n;
+int primes[N], cnt;
+bool st[N];
+
+void get_primes(int n){
+    for(int i = 2; i <= n; i++){
+        if(!st[i]){
+            primes[cnt++] = n;
+            for(int j = i + i; j <= n; j += i) st[j] = true;
+        }
+    }
+}
+
+int main(){
+    cin >> n;
+    get_primes(n);
+    cout << cnt << endl;
+    return 0;
+}
